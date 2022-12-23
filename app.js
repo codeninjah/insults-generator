82189ef6f97bc82bc284f27c4037645cde3dc653
codeninjah <codeninjahblog@gmail.com>
2022-12-23 19:16:41 +0100

    Added insults view, where you can add your own

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express")
-const fs = require('fs/promises')
-const {existsSync} = require('fs')
-const { nextTick } = require("process")
+const fs = require('fs')
 
 const app = express()
 
 app.set("view engine", "ejs")
+app.use( express.static('public') )
 app.use( express.urlencoded( {extended: true } ))
 
 let lines = []
@@ -14,7 +13,7 @@ let random = Math.floor(Math.random() * 30)
 let newArray
 
 const readFile = async (req, res, next) =>{
-    const fileContent = await fs.readFile('insults.txt', {encoding: 'utf8'})
+    const fileContent =  fs.readFileSync('insults.txt', {encoding: 'utf8'})
     lines = fileContent.split("\n")
 
     console.log("Alex was here")
@@ -37,19 +36,37 @@ const readFile = async (req, res, next) =>{
     next()
 }
 
+app.get("/insults", (req, res) => {
+    res.render('insults')
+})
+
+app.post("/insults", (req, res) => {
+    console.log(req.body)
+    console.log("Req body insult is: " + req.body.insult)
+    console.log("Req body severity is: " + req.body.severity)
+    let data = "\n\n" + req.body.insult
+    fs.appendFileSync('insults.txt', data)
+    res.redirect("/")
+})
+
 app.get("/", readFile, (req, res) => {
     console.log("Successful?")
     const randomArrayIndex = Math.floor(Math.random() * newArray.length)
     console.log(newArray[randomArrayIndex])
+
+    //The following line prints the insult
+    res.send(newArray[randomArrayIndex])
 })
 
 app.get("/:severity", (req, res) => {
     console.log(req.params.severity)
     const insult = newArray.find((element) => element.severity == req.params.severity)
-    console.log("Insult is: " + insult.insult)
-    res.send(insult.insult)
+    console.log("Insult is: " + insult) //undefined
+    res.send(insult)
     //res.send("Severity is: " + req.params.severity)
 })
+
+
 
 app.listen(8000, () => {
     console.log("App startad")
